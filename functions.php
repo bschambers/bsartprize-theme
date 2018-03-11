5d387237264f21b0635bd6bc7ccc3c5aec944c35
BS Chambers <ben@bschambers.info>
2018-03-11 14:30:48 +0000

    Added dashboard theme-support help box (functions.php)

--- functions.php
+++ functions.php
@@ -23,4 +23,28 @@ if (function_exists('add_theme_support')) {
     /* add_theme_support('post-formats', array('award', 'news'));*/
 }
 
+
+/*-------------------- CUSTOM DASHBOARD WIDGETS --------------------*/
+
+add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+
+function my_custom_dashboard_widgets() {
+
+    // Globalize the metaboxes array, this holds all the widgets for wp-admin
+    global $wp_meta_boxes;
+
+    wp_add_dashboard_widget('custom_help_widget', 'bsnewsfeed theme support', 'custom_dashboard_help');
+}
+
+function custom_dashboard_help() {
+    //echo '<p><strong>Tips for using the BS Portfolio theme</strong></p>
+    echo '
+<p><strong>POST EXCERPTS FOR HOME PAGE:</strong></p>
+<ul style="list-style-type: disc;">
+
+<li>Use the insert-read-more-tag option in the WYSIWYG editor, (Ctrl + Alt + T).</li>
+
+</ul>';
+}
+
 ?>
