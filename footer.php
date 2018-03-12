134ddca47377a32362d2793bf764856ce0dc0eab
BS Chambers <ben@bschambers.info>
2018-03-12 17:23:03 +0000

    Footer items flow as inline-block - nicer on narrow screen

--- footer.php
+++ footer.php
@@ -1,7 +1,13 @@
 </div><!-- content -->
 
 <footer id="colophon" class="site-footer" role="contentinfo">
-    <p><?php bloginfo('name') ?> --- <?php bloginfo('description') ?> --- CONTACT: <a href="mailto:<?php bloginfo('admin_email') ?>"><?php bloginfo('admin_email') ?></a> --- website built with Wordpress</p>
+    <span class="footer-item"><?php bloginfo('name') ?></span>
+    ---
+    <span class="footer-item"><?php bloginfo('description') ?></span>
+    ---
+    <span class="footer-item">CONTACT: <a href="mailto:<?php bloginfo('admin_email') ?>"><?php bloginfo('admin_email') ?></a></span>
+    ---
+    <span class="footer-item">website built with Wordpress</span>
 </footer><!-- .site-footer -->
 
 <?php wp_footer(); ?>
